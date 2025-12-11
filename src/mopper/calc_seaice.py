1c6f3a628746f1891b309fca3ae223bfd78082d6
pxp900 <paola.petrelli@anu.edu.au>
2025-12-12 08:54:35 +1100

    updates to conda

--- src/mopper/calc_seaice.py
+++ src/mopper/calc_seaice.py
@@ -31,12 +31,8 @@
 
 
 import xarray as xr
-import os
-import json 
 import numpy as np
-import dask
 import logging
-from metpy.calc import height_to_geopotential 
 from importlib.resources import files as import_files
 
 from mopdb.utils import read_yaml, MopException
